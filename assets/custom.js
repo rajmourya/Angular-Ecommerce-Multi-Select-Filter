d90bdc8c4b214687c83ade5f4e08cc56b2d9820c
rajendra mourya <42363801+rajmourya@users.noreply.github.com>
2018-10-03 16:41:24 +0530

    Add files via upload

---
+++ assets/custom.js
@@ -0,0 +1,19 @@
+
+
+          
+$(document).ready(function () {
+	"use strict";
+
+	
+
+	setTimeout(function() {
+		$('#myModal').modal('show');
+	  },1000);  
+	  //document.getElementById("reviewNum").innerText = Math.floor(Math.random() * 99) + 20;
+	  //reviewNum
+	  //let rw =  Math.floor(Math.random() * 99) + 20;
+    //	this.reviewNum = rw;
+
+});
+
+
